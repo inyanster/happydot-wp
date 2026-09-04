51406cf1dac58fa9bd1e9556937492629673af55
tacgithubadmin <tacgithubadmin@straitsinteractive.com>
2026-09-04 10:04:28 +0800

    chore: bump FLEXCORE_SERVER_VERSION to 1.0.8 (cache-bust css icon change)

--- flexcore-server.php
+++ flexcore-server.php
@@ -28,7 +28,7 @@ if (!defined('WPINC')) {
 /**
  * Currently plugin version.
  */
-define('FLEXCORE_SERVER_VERSION', '1.0.7');
+define('FLEXCORE_SERVER_VERSION', '1.0.8');
 
 /**
  * The code that runs during plugin activation.
